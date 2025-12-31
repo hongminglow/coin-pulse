d667640844c4b1866c7e6957f29797524060aade
ming <hongming_1212@hotmail.com>
2026-01-01 01:51:07 +0800

    feat: added necessary assets and setting up app configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 const Page = () => {
-  return <div></div>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <p>Coin Overview</p>
+
+        <p>Trending Coins</p>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
