4741559b1182f71ed741eeeaaa747eae81e020ec
ming <hongming_1212@hotmail.com>
2026-01-01 01:37:08 +0800

    feat: added simple header and navigaton

---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "../lib/utils";
+
+export const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href="/">
+          <Image
+            src="/assets/logo.svg"
+            alt="Coin Pulse Logo"
+            width={132}
+            height={40}
+          />
+        </Link>
+
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathname === "/",
+              "is-home": true,
+            })}
+          >
+            Home
+          </Link>
+
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn("nav-link", {
+              "is-active": pathname === "/coins",
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
